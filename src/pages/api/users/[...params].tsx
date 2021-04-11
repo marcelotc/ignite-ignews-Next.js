61d69db8530d184313e4ae0c534b34b98add2361
Marcelo Teixeira Cortes <marceloteixeiracortes@gmail.com>
2021-04-11 16:33:47 -0300

    Parametrização nas rotas

---
+++ src/pages/api/users/[...params].tsx
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    console.log(request.query)
+
+    const users = [
+        { id: 1, name: 'Marcelo' },
+        { id: 2, name: 'cortes' },
+        { id: 3, name: 'te' },
+    ]
+
+    return response.json(users);
+}
